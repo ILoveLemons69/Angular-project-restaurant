bdd258d317e54fa95f8f07436a3e6a54fdab25a8
snaiperigurama <nibladzeguram123@gmail.com>
2026-05-09 21:41:23 +0400

    made header and made paths for all components.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Cart } from './cart/cart';
+import { Menu } from './menu/menu';
+import { LogIn } from './log-in/log-in';
+import { SignUp } from './sign-up/sign-up';
+import { Profile } from './profile/profile';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : 'home',
+        component: Home
+    },
+    {
+        path : 'cart',
+        component: Cart
+    },
+    {
+        path : 'menu',
+        component: Menu
+    },
+    {
+        path : 'log-in',
+        component: LogIn
+    },
+    {
+        path : 'sign-up',
+        component: SignUp
+    },
+    {
+        path : 'profile',
+        component: Profile
+    },
+    {
+        path : '**',
+        redirectTo: 'home'
+    }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { Header } from './header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterModule, Header],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
