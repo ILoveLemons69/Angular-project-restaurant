4b4f1b7d87f9298756d1bf7a1286614dfb2603b4
snaiperigurama <nibladzeguram123@gmail.com>
2026-05-10 15:07:56 +0400

    added animations, made footer and styled home page.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Header } from './header/header';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterModule, Header],
+  imports: [RouterModule, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
